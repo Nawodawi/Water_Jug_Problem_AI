82d529e8fb48c5ad4cf3b8557900cf953479f70e
Lakshith95 <lucky.senanayake@gmail.com>
2021-05-23 20:47:08 +0530

    Adding get_goal and get_jugs fuctions and initiating search funtion

--- Water_Jug_Problem_BFS.py
+++ Water_Jug_Problem_BFS.py
@@ -3,6 +3,9 @@ import collections
 def main():                                    
     starting_node = [[0, 0]]    #Initial stage is defined
     jugs = get_jugs()           #Assigning user inputs for jug capacities
+    goal_amount = get_goal(jugs)    #Assigning the target volume user needed
+    check_dict = {}                 #a dictionary storing visited nodes
+    search(starting_node, jugs, goal_amount, check_dict)    #Calling search function with attributes
    
 
 def get_jugs():
@@ -20,6 +23,28 @@ def get_jugs():
     jugs.append(temp)
     
     return jugs                                              #returning jug capcities
+
+
+def get_goal(jugs):
+
+    max_amount = max(jugs[0], jugs[1])
+    s = "Enter the desired amount of water (1 - {0}): ".format(max_amount)  #Getting the user input for the target volume of water
+    goal_amount = int(input(s))
+    while goal_amount < 1 or goal_amount > max_amount:                      #Validating the target amount of water
+        goal_amount = int(input("Enter a valid amount (1 - {0}): ".format(max_amount)))
+        
+    return goal_amount              #Returns desired amount of water.
+
+
+def search(starting_node, jugs, goal_amount, check_dict):  #searching for a path between starting node and goal node
+ 
+    goal = []
+    accomplished = False
+    
+    q = collections.deque()         #A deque is a double-ended queue. It can be used to add or remove elements from both ends.
+    q.appendleft(starting_node)     #inserting the value in its argument to the left end of deque
+    
+           
 
 
 
