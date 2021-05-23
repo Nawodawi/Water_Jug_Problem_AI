a932e307d15a9f8d660dc916bbef1cd8ac16fcee
Lakshith95 <lucky.senanayake@gmail.com>
2021-05-23 13:31:32 +0530

    Adding Jug class And GCD

---
+++ JugClass.py
@@ -0,0 +1,11 @@
+class JugState:
+    capacity = 0               #defining variables
+    currentVol = 0
+    name = 'new jug'
+
+def __init__(self, capacity, name):     #initiating function
+        self.capacity = capacity
+        self.name = name
+        self.currentVol = 0
+
+
--- Water_Jug_Problem.py
+++ Water_Jug_Problem.py
@@ -1,4 +1,6 @@
+import JugClass
 import sys
+
 
 jugVolume1 = int(input('Jug 1 Volume:'), 10)     #getting user input for the volume of jug one
 jugVolume2 = int(input('Jug 2 Volume:'), 10)     ##getting user input for the volume of jug two
@@ -15,4 +17,21 @@ if targetVolume > jugVolume1 and targetVolume > jugVolume2:    #Target volume ca
     sys.exit(1)
 
 
+smaller = JugClass.JugState(min(jugVolume1, jugVolume2), 'Jug 1')   #Assigning minimum volume jug as smaller
+
+
+larger = JugClass.JugState(max(jugVolume1, jugVolume2), 'Jug 2')    #Assigning maximum volume jug as larger
+
+def getGCD(a, b):          #Calculating the gratest common divisor of vulumes of two jugs
+    larger = max(a, b)
+    smaller = min(a, b)
+    if larger != smaller:
+        smaller = getGCD(larger - smaller, smaller)
+    return smaller
+
+gcd = getGCD(smaller.capacity, larger.capacity)  #Assigning gcd
+
+if targetVolume % gcd != 0:                      #Checking for a exception
+    print('No possible Solution with these volumes')
+    sys.exit(0)   
 
